d9c9cf2d72fc58140ba63f23a6a5627986df7369
jcarlson <jake.carlson90@gmail.com>
2024-05-13 11:59:31 -0700

    DB connection

--- server/app.js
+++ server/app.js
@@ -2,9 +2,30 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import storyRoutes from './routes/stories.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "32mb", extended}));
+app.use(bodyParser.json({ limit: "32mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "32mb", extended: true}));
 app.use(cors());
+app.use("/stories", storyRoutes);
+
+const MONGO_URI= "mongodb+srv://Jake:5b0aNo2COtHDUXsz@cluster0.inwkazg.mongodb.net/api-project?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5001;
+
+const connectDB = async () => {
+
+    try {
+        await mongoose.connect(MONGO_URI)
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    } catch (err) {
+        console.error("connection failed", err.message);
+
+    }
+}
+
+connectDB();
+
+mongoose.connection.on("open", () => console.log("connection to database successful"));
+mongoose.connection.on("error", (err) => console.log(err));
